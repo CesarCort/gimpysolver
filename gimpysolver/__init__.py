0925b01e646b6394f5cef362ca65964af7e22481
Cesar Cort <cesarunner3@gmail.com>
2022-02-07 17:07:54 -0500

    https://pypi.org/project/gimpysolver/ first functional version

--- gimpysolver/__init__.py
+++ gimpysolver/__init__.py
@@ -2,8 +2,8 @@
 Captcha solver to SEACE Portal Scraper
 The model have a 92% Accuracy.
 """
-from gimpysolver import captcha_solver
+#from gimpysolver import captcha_solver
 
-__version__ = "0.0.1"
+__version__ = "0.0.8"
 __author__ = 'César Cortez'
 __credits__ = 'Digitalia Tec'
--- setup.py
+++ setup.py
@@ -1,29 +1,40 @@
+import os
+import re
+import inspect
 
 from distutils.core import setup
+import setuptools
+
+def get_path():
+    globalPath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    return globalPath
+
+def read(fname):
+    __file__ = get_path()
+    return open(os.path.join(__file__, fname)).read()
 
 with open("README.rst", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
   name = 'gimpysolver',         # How you named your package folder (MyLib)
-  packages = ['gimpysolver'],   # Chose the same as "name"
-  version = '0.0.2',      # Start with a small number and increase it with every change you make
+  #packages = setuptools.find_packages(where="gimpysolver"),   # Chose the same as "name"
+  version = '0.0.14',      # Start with a small number and increase it with every change you make
   license='gpl-3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'This is a simple and specific solver to gimpy captcha types like SEACE PORTAL. The model has been trained with 4000 images and 92 porcent of accuracy.',   # Give a short description about your library
-  long_description=long_description,
+  description = ('This is a simple and specific solver to gimpy captcha types like SEACE PORTAL. The model has been trained with 4000 images and 92 porcent of accuracy.'),   # Give a short description about your library
+  long_description=read("README.rst"),
   long_description_content_type = "text/markdown",
-  author = 'César Cortez',                   # Type in your name
+  author = 'Cesar Cortez',                   # Type in your name
   author_email = 'cc@digitaliatec.com',      # Type in your E-Mail
-  url = 'https://github.com/CesarCort/gimpysolver',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/CesarCort/gimpysolver/archive/refs/heads/main.zip',    # I explain this later on
+  url = "http://github.com/CesarCort/gimpysolver",   # Provide either the link to your github or to your website
+  project_urls={
+        "Bug Tracker": "http://github.com/CesarCort/gimpysolver/issues",
+    },
   keywords = ['CAPTCHA', 'CAPTCHASOLVER', 'GIMPY','GIMPYSOLVER','SOLVER'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'keras',
-          'io',
-	   'PIL',
-	'opencv-python',
-'importlib',
-'io'
+         'keras==2.7.0',
+	'opencv-python==4.5.1.48',
+	'importlib==1.0.4'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
